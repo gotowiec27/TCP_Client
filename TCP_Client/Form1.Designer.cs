9669362d3ebea9f0fa750a456b5db54e8dd4ab0d
Adam Toda <adam368@poczta.onet.pl>
2017-12-21 17:27:14 +0100

    Dodanie flagi. Zmiana treści przycisku po naciśnięciu i nawiązaniu połączenia. Dodanie informacji o zakończeniu połączenia.

--- TCP_Client/Form1.Designer.cs
+++ TCP_Client/Form1.Designer.cs
@@ -29,11 +29,107 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.ip_box = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.port_box = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.send_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.port_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ip_box
+            // 
+            this.ip_box.Location = new System.Drawing.Point(39, 12);
+            this.ip_box.Name = "ip_box";
+            this.ip_box.Size = new System.Drawing.Size(194, 22);
+            this.ip_box.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // port_box
+            // 
+            this.port_box.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.port_box.Location = new System.Drawing.Point(283, 13);
+            this.port_box.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.port_box.Name = "port_box";
+            this.port_box.Size = new System.Drawing.Size(98, 22);
+            this.port_box.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(490, 132);
+            this.listBox1.TabIndex = 3;
+            // 
+            // send_button
+            // 
+            this.send_button.Location = new System.Drawing.Point(401, 13);
+            this.send_button.Name = "send_button";
+            this.send_button.Size = new System.Drawing.Size(101, 23);
+            this.send_button.TabIndex = 4;
+            this.send_button.Text = "Connect";
+            this.send_button.UseVisualStyleBackColor = true;
+            this.send_button.Click += new System.EventHandler(this.send_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Port:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 201);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.send_button);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.port_box);
+            this.Controls.Add(this.ip_box);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.port_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ip_box;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NumericUpDown port_box;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button send_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- TCP_Client/Form1.cs
+++ TCP_Client/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net.Sockets;
 namespace TCP_Client
 {
     public partial class Form1 : Form
     {
+        TcpClient klient;
+        bool is_clicked = false;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void send_button_Click(object sender, EventArgs e)
+        {
+
+            if (!is_clicked)
+            {
+                
+                string host = ip_box.Text;
+                int port = System.Convert.ToInt16(port_box.Value);
+                try
+                {
+                    klient = new TcpClient(host, port);
+                    listBox1.Items.Add("Nawiązano połączenie z " + host + " na porcie:" + port);
+                    is_clicked = true;
+                    send_button.Text = "Disconnect";
+                }
+
+                catch (Exception ex)
+                {
+                    listBox1.Items.Add("Błąd: Nie udało się nawiązać połączenia!");
+                }
+
+            }
+            else
+            {
+
+                klient.Close();
+                listBox1.Items.Add("Zakończono połączenie.");
+                is_clicked = false;
+                send_button.Text = "Connect";
+            }
+            
         }
     }
 }
